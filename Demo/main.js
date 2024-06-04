7c2f6e80c871d6d109a9ac68b93f4e8ca7fcf51d
unknown <tobiawolaju21@gmail.com>
2024-06-04 12:21:12 +0100

    hk.glhiss;ls: Please enter the commit message for your changes. Lines starting

---
+++ Demo/main.js
@@ -0,0 +1,30 @@
+import { api_key, storage_directory, captureframe } from '../SDK/ls.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = "your_api_key_here";
+    const storageDir = "your_storage_directory_here";
+
+    api_key(apiKey);
+    storage_directory(storageDir);
+
+    const captureButton = document.getElementById('captureButton');
+    captureButton.addEventListener('click', captureframe);
+});
+
+
+//camera stream
+const video = document.getElementById('camera-stream');
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+      video.srcObject = stream;
+    })
+    .catch(error => {
+      console.error('Error accessing camera:', error);
+    });
+} else {
+  console.error('Your browser does not support getUserMedia');
+}
+
+
